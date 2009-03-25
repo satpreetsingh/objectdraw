f325cb253def75a8083f8e86d4201528cd2246e6
satpreetsingh <satpreetsingh@176ab4a0-13d1-11de-99d6-5fb3f1362b5f>
2009-03-25 15:22:40 +0000

    Bounding box for select for LineObj

--- trunk/ObjectDraw1/objectdraw1/LineShapeObj.java
+++ trunk/ObjectDraw1/objectdraw1/LineShapeObj.java
@@ -29,9 +29,38 @@ public class LineShapeObj implements TwoEndShapeObj{
 	  	g.drawLine(startp.x, startp.y, endp.x, endp.y);
 	  	
 	  	if (this.isHighlighted == true) {
-		    g.setColor(Color.GRAY);
-		  	g.drawLine(startp.x + 2, startp.y, endp.x + 2, endp.y);	  		
-		  	g.drawLine(startp.x - 2, startp.y, endp.x - 2, endp.y);	  		
+		    g.setColor(Color.lightGray);
+		    // Set up bounding box
+			int x0 = this.startp.x;
+			int y0 = this.startp.y;
+			int x1 = this.endp.x;
+			int y1 = this.endp.y;
+			 
+			int shapeX;
+		    int shapeY;
+		    int shapeWidth;
+		    int shapeHeight;
+		    if (x0 <= x1) {
+		      shapeX = x0;
+		      shapeWidth = (x1-x0)+1;
+		    }
+		    else {
+		      shapeX = x1;
+		      shapeWidth = (x0-x1)+1;
+		    }
+		    
+		    if (y0 <= y1) {
+		      shapeY = y0;
+		      shapeHeight = (y1-y0)+1;
+		    }
+		    else {
+		      shapeY = y1;
+		      shapeHeight = (y0-y1)+1;
+		    }
+		    
+		    g.drawRect(shapeX - 2, shapeY - 2, shapeWidth + 4, shapeHeight + 4);
+//		    g.drawLine(startp.x + 2, startp.y, endp.x + 2, endp.y);	  		
+//		  	g.drawLine(startp.x - 2, startp.y, endp.x - 2, endp.y);	  		
 	  	}
 	  }  
 
