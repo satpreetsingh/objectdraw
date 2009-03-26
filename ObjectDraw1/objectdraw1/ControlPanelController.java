3ae150f80ec9633cca7f76dc96e37a1ea0d6b254
satpreetsingh <satpreetsingh@176ab4a0-13d1-11de-99d6-5fb3f1362b5f>
2009-03-26 20:34:31 +0000

    Got select-color-change to work

--- ObjectDraw1/objectdraw1/ControlPanelController.java
+++ ObjectDraw1/objectdraw1/ControlPanelController.java
@@ -4,40 +4,50 @@ import java.awt.*;
 import java.awt.event.*;
 
 public class ControlPanelController
-	implements ActionListener, ItemListener {
-;
-  protected DrawingCanvas canvas;
-  
-  ControlPanelController(DrawingCanvas c) {
-    canvas = c;
-  }
+implements ActionListener, ItemListener {
+	;
+	protected DrawingCanvas canvas;
 
-  public void actionPerformed(ActionEvent e) {
-	System.out.println("actionPerformed in ControlPanelController -> [Clear]");  
-    canvas.clearCanvas();
-    canvas.clearObjs();
-  }
+	ControlPanelController(DrawingCanvas c) {
+		canvas = c;
+	}
 
-  public void itemStateChanged(ItemEvent e)  {
-    if (e.getStateChange() == ItemEvent.SELECTED) {
-      canvas.setpenColor(itemToColor(e.getItem()));
-    }
-  }
+	public void actionPerformed(ActionEvent e) {
+		System.out.println("actionPerformed in ControlPanelController -> [Clear]");  
+		canvas.clearCanvas();
+		canvas.clearObjs();
+	}
 
-  protected Color itemToColor(Object item) {
-    
-    if("black".equals(item)) {
-      return Color.black;
-    }
-    else if("blue".equals(item)) {
-          return Color.blue;
-         }
-         else if("green".equals(item)) {
-               return Color.green;
-              }
-              else {
-                return Color.red;
-              }
-  }
+	public void itemStateChanged(ItemEvent e)  {
+		if (e.getStateChange() == ItemEvent.SELECTED) {
+			canvas.setpenColor(itemToColor(e.getItem()));
+
+		if (canvas.highlightedObj != null) {
+			System.out.println("From ControlPanelController: changing object color");
+			canvas.highlightedObj.setColor(itemToColor(e.getItem())); 			 
+			canvas.clearCanvas();
+			canvas.redrawObjs();
+			canvas.highlightedObj.drawObjBoundingBox(canvas.getimageBufferGraphics());
+		}
+
+
+		}
+	}
+
+	protected Color itemToColor(Object item) {
+
+		if("black".equals(item)) {
+			return Color.black;
+		}
+		else if("blue".equals(item)) {
+			return Color.blue;
+		}
+		else if("green".equals(item)) {
+			return Color.green;
+		}
+		else {
+			return Color.red;
+		}
+	}
 
 } 
--- ObjectDraw1/objectdraw1/TwoEndShapeObj.java
+++ ObjectDraw1/objectdraw1/TwoEndShapeObj.java
@@ -3,30 +3,22 @@ package objectdraw1;
 
 import java.awt.*;
 
-public interface TwoEndShapeObj {
+public abstract class TwoEndShapeObj {
 
-  void setColor(Color objColor);	
+	private Point startp = new Point();
+	private Point endp = new Point(); 
+	private Color objColor;
+	private boolean isHighlighted = false;
 
-  void setEndPoints(Point startp, Point endp);
-		
-  void drawObj(Graphics g);
+	abstract void setColor(Color objColor);	
+	abstract void setEndPoints(Point startp, Point endp);		
+	abstract void drawObj(Graphics g);
+	abstract void drawOutline(Graphics g, int x0, int y0, int x1, int y1);
+	abstract void move(int delX, int delY);
+	abstract boolean isPointInObject(Point p);
+	abstract void drawObjBoundingBox(Graphics g);
 
-  void drawOutline(Graphics g, int x0, int y0,
-		      int x1, int y1);
-  
-  void move(int delX, int delY);
+	//abstract void resize();  
+	//int isPointInObjectCorner(Point p);
 
-  boolean isPointInObject(Point p);
-
-  void drawObjBoundingBox(Graphics g);
-
-//  @deprecated 
-//  void setHighlighted();
-//  void clearHighlighted();
-//  boolean getHighlighted();
-  
-  //void resize();
-  
-  //int isPointInObjectCorner(Point p);
-  
 }
